38f0fb482ab6314c6e1a2ac9bfc87ef8bbe26534
Graham Rounds <grounds@lifespan.org>
2016-02-19 15:27:10 -0500

    test(ApiService): rewrote a few tests to use backend mocks

--- src/app/services/api/api.spec.ts
+++ src/app/services/api/api.spec.ts
@@ -1,4 +1,4 @@
-import {provide} from 'angular2/core';
+import {provide, Injector} from 'angular2/core';
 import {
   it,
   describe,
@@ -8,12 +8,100 @@ import {
   beforeEachProviders
 } from 'angular2/testing';
 import {MockBackend} from 'angular2/http/testing';
-import {ConnectionBackend, Http, Headers, HTTP_PROVIDERS, Response, ResponseOptions, XHRBackend} from 'angular2/http';
+import {BaseRequestOptions, ConnectionBackend, Http, Headers, HTTP_PROVIDERS, Response, ResponseOptions, XHRBackend} from 'angular2/http';
 
 import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 import {ApiService} from './api';
 import {ApiOptions} from './api.options';
+
+describe('ApiService', () => {
+  let http: Http;
+  let response = new Response(new ResponseOptions({body: 'bar'}));
+  let storage = {
+    'testJwt': 'foobar'
+  };
+
+  beforeEachProviders(() => [
+    MockBackend,
+    BaseRequestOptions,
+    provide(Http, {useFactory: (backend, defaultOptions) => new Http(backend, defaultOptions), deps: [MockBackend, BaseRequestOptions]
+    }),
+    provide(ApiOptions, {useValue: new ApiOptions({
+      baseUrl: 'https://foo.bar/',
+      jwtName: 'testJwt'
+    })}),
+    ApiService
+  ]);
+
+  beforeEach(inject([Http, MockBackend], (_http: Http, mockBackend: MockBackend) => {
+    http = _http;
+    spyOn(http, "get").and.callThrough();
+    spyOn(http, "post").and.callThrough();
+    spyOn(http, "put").and.callThrough();
+
+    //set the mockbackend response
+    mockBackend.connections.subscribe(connection => connection.mockRespond(response));
+
+    //mock localStorage for token retrievals
+    spyOn(localStorage, 'getItem').and.callFake(function(key) {
+      return storage[key];
+    })
+  }));
+
+  describe('with custom token', () => {
+    let authHeader = new Headers({
+      'authorization': 'Bearer jwt'
+    });
+    let jsonHeader = new Headers({
+      'Content-Type': 'application/json'
+    });
+
+    it('should call http.get and return some data', inject([ApiService], (api: ApiService) => {
+      api.get('https://foo.bar', 'jwt')
+        .map((data: Response) => data.text())
+        .subscribe(data => {
+          expect(data).toBe('bar');
+        });
+
+      expect(http.get).toHaveBeenCalledWith('https://foo.bar', {headers: authHeader});
+    }));
+
+  }); //end 'with custom token'
+
+  describe('without custom token', () => {
+    let authHeader = new Headers({
+      'authorization': 'Bearer foobar'
+    });
+    let jsonHeader = new Headers({
+      'Content-Type': 'application/json'
+    });
+
+    it('should get some data', inject([ApiService], (api: ApiService) => {
+      api.get('https://foo.bar')
+        .map((data: Response) => data.text())
+        .subscribe(data => {
+          expect(data).toBe('bar');
+        });
+
+      expect(http.get).toHaveBeenCalledWith('https://foo.bar', {headers: authHeader});
+    }));
+
+    it("should login and return some data", inject([ApiService], (api:ApiService) => {
+      let credentials = {email: 'foo@bar.com', password: 'hunter2'};
+      api.login(credentials)
+        .map((data: Response) => data.text())
+        .subscribe(data => {
+          expect(data).toBe('bar');
+        });
+
+      expect(http.post).toHaveBeenCalledWith('https://foo.bar/auth/login', JSON.stringify(credentials), {headers: jsonHeader});
+    }));
+
+  }); //end 'without custom token'
+
+}); //end describe('ApiService')
 
 describe('Simple GET Request', () => {
   let http:Http;
@@ -34,26 +122,7 @@ describe('Simple GET Request', () => {
     spyOn(http, "put").and.returnValue("PUT response");
   }));
 
-  describe('ApiService.get()', () => {
-    it("should call http.get and return the raw response", inject([ApiService], (api:ApiService) => {
-      let response = api.get('https://foo.bar', 'jwt');
-      let headers = new Headers({
-        'authorization': 'Bearer jwt'
-      });
-      expect(http.get).toHaveBeenCalledWith('https://foo.bar', { 'headers': headers});
-      expect(response).toBe("GET response");
-    }));
-  });
 
-  describe('ApiService.login()', () => {
-    it("should call http.post and return the raw response", inject([ApiService], (api:ApiService) => {
-      let credentials = {email: 'foo@bar.com', password: 'hunter2'};
-      let response = api.login(credentials);
-      let headers = {headers: new Headers({'Content-Type': 'application/json'})};
-      expect(http.post).toHaveBeenCalledWith('https://api.com/auth/login', JSON.stringify(credentials), headers);
-      expect(response).toBe('POST response');
-    }))
-  });
 
   describe('ApiService.register()', () => {
     it('should call http.post and return the raw response', inject([ApiService], (api:ApiService) => {
